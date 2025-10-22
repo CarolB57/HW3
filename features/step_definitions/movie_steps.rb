bd42eee424192f798556a905e0db23607ea8151c
Caroline Bohadana <232050975@aluno.unb.br>
2025-10-22 11:08:51 -0300

    User stories funcionando para sort_movie_list.feature

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -26,3 +26,19 @@ Then /I should see all of the movies/ do
   # A verificação final continua a mesma.
   expect(actual_count).to eq(expected_count)
 end
+Then /I should see "(.*)" before "(.*)"/ do |movie1, movie2|
+  # 1. Encontra todos os elementos que contêm os títulos dos filmes, na ordem em que aparecem na página.
+  #    Este seletor CSS procura por divs com a classe 'col-8' dentro do div com id 'movies'.
+  movie_titles = page.all('#movies .row .col-8').map(&:text)
+
+  # 2. Verifica a posição (índice) de cada filme na lista de títulos que extraímos.
+  index1 = movie_titles.index(movie1)
+  index2 = movie_titles.index(movie2)
+
+  # 3. Se um dos filmes não foi encontrado na lista, falha o teste.
+  raise "#{movie1} not found on page" unless index1
+  raise "#{movie2} not found on page" unless index2
+
+  # 4. O teste passa se o índice do primeiro filme for menor que o do segundo.
+  expect(index1).to be < index2
+end
